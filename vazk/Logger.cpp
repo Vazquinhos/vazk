45e074372dcef38a06b89ce362b6328a03415bc8
Vazquinhos <vazquinhos@gmail.com>
2017-12-18 15:34:19 +0100

    Trace message improved

--- vazk/Logger.cpp
+++ vazk/Logger.cpp
@@ -3,6 +3,9 @@
 #include "EnumStringConversor.hpp"
 
 #include <cstdarg>
+#include <string>
+#include <stdio.h>
+#include <Windows.h>
 
 namespace Logger
 {
@@ -15,7 +18,7 @@ namespace Logger
       Register_Enum_String(Logger::Severity::eFatal, "Fatal");
       Register_Enum_String(Logger::Severity::eError, "Error");
       Register_Enum_String(Logger::Severity::eWarning, "Warning");
-      Register_Enum_String(Logger::Severity::eInfo, "Info");
+      Register_Enum_String(Logger::Severity::eInfo, "Information");
     }
     End_Enum_String;
   }
@@ -23,8 +26,12 @@ namespace Logger
   void Trace(Severity _severity, const char* _file, uint32_t _line, const std::string& _msg)
   {
     std::string lFullMsg = std::string("[") + EnumString<Logger::Severity>::asString(_severity) + std::string("]");
-    lFullMsg += EnumString<Logger::Severity>::asString(_severity) + std::string("]");
-    sTraceFunction(_severity, lFullMsg);
+    lFullMsg += _msg;
+    lFullMsg += std::string("[") + _file + std::string("]");
+    lFullMsg += std::string("[") + std::to_string(_line) + std::string("]\n");
+
+    if (sTraceFunction != nullptr)
+      sTraceFunction(_severity, lFullMsg);
   }
 
   void BindOutputFunction(std::function<void(Severity, const std::string&)> _fun)
